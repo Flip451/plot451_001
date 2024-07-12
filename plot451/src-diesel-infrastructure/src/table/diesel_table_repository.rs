07bcaffd68a18d3e77aa39dd26d77c349d86bf16
Flip451 <flipflap451@gmail.com>
2024-07-12 23:00:43 +0900

    feat: impl TableRepository

--- plot451/src-diesel-infrastructure/src/table/diesel_table_repository.rs
+++ plot451/src-diesel-infrastructure/src/table/diesel_table_repository.rs
@@ -1,8 +1,58 @@
-use std::sync::{Arc, RwLock};
+use diesel::{
+    r2d2::{ConnectionManager, PooledConnection},
+    SqliteConnection,
+};
+use src_domain::models::table::{
+    table::Table,
+    table_id::TableId,
+    table_repository::{ITableRepository, TableRepositoryError, TableRepositoryResult},
+};
 
-use diesel::SqliteConnection;
+use crate::db::SqlitePool;
 
 #[derive(Clone)]
 pub struct DieselTableRepository {
-    connection: Arc<RwLock<SqliteConnection>>
+    pool: SqlitePool,
+}
+
+impl DieselTableRepository {
+    pub fn new(pool: SqlitePool) -> Self {
+        Self { pool }
+    }
+
+    async fn connection(
+        &self,
+    ) -> TableRepositoryResult<PooledConnection<ConnectionManager<SqliteConnection>>> {
+        self.pool
+            .get()
+            .map_err(|e| TableRepositoryError::Unexpected(e.to_string()))
+    }
+}
+
+impl ITableRepository for DieselTableRepository {
+    async fn save(&self, table: &Table) -> TableRepositoryResult<TableId> {
+        Ok(todo!())
+    }
+
+    async fn find(&self, id: &TableId) -> TableRepositoryResult<Option<Table>> {
+        todo!()
+    }
+
+    async fn find_parent_table_by_column_id(
+        &self,
+        column_id: &src_domain::models::column::column_id::ColumnId,
+    ) -> TableRepositoryResult<Vec<Table>> {
+        Ok(todo!())
+    }
+
+    async fn find_all(&self) -> TableRepositoryResult<Vec<Table>> {
+        Ok(todo!())
+    }
+
+    async fn delete(
+        &self,
+        table: src_domain::models::table::table::Table,
+    ) -> TableRepositoryResult<()> {
+        Ok(todo!())
+    }
 }
